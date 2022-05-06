e7aa23dd18ee0e2694fc219dafeee46d5b69d89b
Laxmaiah <Laxmaiah@Laxmaiah-PC>
2022-05-06 09:27:48 +0530

    dgas

---
+++ src/com/contacts/Con1.java
@@ -0,0 +1,10 @@
+package com.contacts;
+
+public class Con1 {
+
+	public static void main(String[] args) {
+		System.out.println("con1 address");
+
+	}
+
+}
---
+++ src/com/contacts/Con2.java
@@ -0,0 +1,10 @@
+package com.contacts;
+
+public class Con2 {
+
+	public static void main(String[] args) {
+		System.out.println("con2 address");
+
+	}
+
+}
